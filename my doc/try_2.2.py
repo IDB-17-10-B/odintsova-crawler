3f1739920dc1a32247ca5047b3b88be012c0acc8
nepetann <43468818+nepetann@users.noreply.github.com>
2018-10-10 21:46:22 +0300

    Update try_2.2.py

--- my doc/try_2.2.py
+++ my doc/try_2.2.py
@@ -4,7 +4,7 @@ depth=2
 import re
 import requests
 import time
-pattern=re.compile(r'href="(?P<url>[a-zA-Z0-9:/&?=/.-]+">[a-zA-Zа-яА-Я0-9:/&?=/.-/ ]+)</a>')
+pattern=re.compile(r'href="(?P<url>[a-zA-Z0-9:/&?=/.-]+">[a-zA-Zа-яА-Я0-9:/&?=/./ /(/)-]+)</a>')
 
 
 
@@ -16,13 +16,13 @@ def foo(addr, index):
   for item in links:
     if item.endswith(('.png','.css')):
       continue
-
+    item_split=['','']
     item_split=re.split(r'">',item,1)
     if len(item_split)>1:
      new_text.append(item_split[1])
     else:
       new_text.append('none')
-
+    #print(new_text,new_links)
     if item.startswith('/'):
       new_links.append (site+item_split[0])
     elif not item.startswith('/') and not item.startswith('http://'):
@@ -36,14 +36,14 @@ def foo(addr, index):
 
   all_links=[]
   all_text=[]
-  item=1
-  while item<len(new_links):
-    print(new_text[item],'-',new_links[item])
+  inumb=0
+  while inumb<len(new_links):
+    print(new_text[inumb],'-',new_links[inumb])
     time.sleep(2)
-    current_file = foo(item,index-1)
+    current_file =foo(new_links[inumb],index-1)
     all_links.extend(current_file[0])
     all_text.extend(current_file[1])
-    item+=1
+    inumb+=1
   new_links.extend(all_links)
   new_text.extend(all_text)
   new_file=[new_links,new_text]
@@ -52,7 +52,7 @@ def foo(addr, index):
 main_file=foo(site,depth)
 main_links.extend(main_file[0])
 main_text.extend(main_file[1])
-item=1
-while item<len(main_links):
-  print (main_text[item],'-',main_links[item])
-  item+=1
+inumb=0
+while inumb<len(main_links):
+  print (main_text[inumb],'-',main_links[inumb])
+  inumb+=1
