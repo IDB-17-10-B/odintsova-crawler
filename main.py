2a641c9157b8be61476eed251f994a6a929259dd
nepetann <43468818+nepetann@users.noreply.github.com>
2018-10-10 21:56:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,59 @@
-
 site='http://stankin.ru'
-
+depth=2
 
 import re
 import requests
 import time
-pattern=re.compile(r'href="(?P<url>[a-zA-Z0-9:/&?=/.-]+)"')
+pattern=re.compile(r'href="(?P<url>[a-zA-Z0-9:/&?=/.-]+">[a-zA-Zа-яА-Я0-9:/&?=/./ /(/)-]+)</a>')
+
 
 
 def foo(addr, index):
   html=requests.get(addr).text
   links=pattern.findall(html)
   new_links=[]
+  new_text=[]
   for item in links:
-    if item.endswith('.png'):
+    if item.endswith(('.png','.css')):
       continue
+    item_split=['','']
+    item_split=re.split(r'">',item,1)
+    if len(item_split)>1:
+     new_text.append(item_split[1])
+    else:
+      new_text.append('none')
+    #print(new_text,new_links)
     if item.startswith('/'):
-      new_links.append (site+item)
+      new_links.append (site+item_split[0])
     elif not item.startswith('/') and not item.startswith('http://'):
-      new_links.append (addr+'/'+item) 
+      new_links.append (addr+'/'+item_split[0]) 
     elif 'stankin.ru' in item:
-      new_links.append(item)
+      new_links.append(item_split[0])
 
   if (index-1<0):
-    return new_links
-  all_links =[]
-  for item in new_links:
-    print(item)
+    new_file=[new_links,new_text]
+    return new_file
+
+  all_links=[]
+  all_text=[]
+  inumb=0
+  while inumb<len(new_links):
+    print(new_text[inumb],'-',new_links[inumb])
     time.sleep(2)
-    current_links = foo(item,index-1)
-    all_links.extend(current_links)
+    current_file =foo(new_links[inumb],index-1)
+    all_links.extend(current_file[0])
+    all_text.extend(current_file[1])
+    inumb+=1
   new_links.extend(all_links)
-  return new_links
-for item in foo(site,2):
-  print (item)
+  new_text.extend(all_text)
+  new_file=[new_links,new_text]
+  return new_file
+
+main_file=foo(site,depth)
+main_links.extend(main_file[0])
+main_text.extend(main_file[1])
+inumb=0
+while inumb<len(main_links):
+  print (main_text[inumb],'-',main_links[inumb])
+  inumb+=1
+
